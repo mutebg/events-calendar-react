59ac27a409f74c8f815eb4d3898e4856ed3800b1
Stoyan Delev <s.delev@elements.nl>
2016-01-12 20:04:29 +0100

    change router

--- app/routes.js
+++ app/routes.js
@@ -4,6 +4,7 @@ import {Router, Route, IndexRoute } from 'react-router';
 //components
 import Layout from './components/Layout';
 import Home from './components/PageHome';
+import Add from './components/PageAdd';
 import Details from './components/PageDetails';
 import Calendar from './components/PageCalendar';
 import List from './components/PageList';
@@ -14,6 +15,7 @@ export default (
     <Route path="/event-:id" component={Details} />
     <Route path="/calendar" component={Calendar} />
     <Route path="/list" component={List} />
+    <Route path="/add" component={Add} />
     <IndexRoute component={Home}/>
   </Route>
 );
